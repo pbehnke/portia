7e8a52afb0f7703a857e19ccd79b2b540122e4e3
Phil <phil.behnke@gmail.com>
2020-03-10 18:16:08 -0400

    Update provision.sh

--- docker/provision.sh
+++ docker/provision.sh
@@ -114,34 +114,35 @@ install_deps () {
 }
 
 install_qtwebkit_deps () {
-    apt-get install -y --no-install-recommends \
-        xvfb \
-        libjpeg-turbo8-dev \
-        libgl1-mesa-dev \
-        libglu1-mesa-dev \
-        mesa-common-dev \
-        libfontconfig1-dev \
-        libicu-dev \
-        libpng12-dev \
-        libxslt1-dev \
-        libxml2-dev \
-        libhyphen-dev \
-        libgbm1 \
-        libxcb-image0 \
-        libxcb-icccm4 \
-        libxcb-keysyms1 \
-        libxcb-render-util0 \
-        libxi6 \
-        libxcomposite-dev \
-        libxrender-dev \
-        libgstreamer1.0-dev \
-        libgstreamer-plugins-base1.0-dev \
-        libgstreamer-plugins-good1.0-dev \
-        gstreamer1.0-plugins-good \
-        gstreamer1.0-x \
-        gstreamer1.0-libav \
-        webp \
-        rsync
+      apt-get install -y qml-module-qtwebkit rsync
+#     apt-get install -y --no-install-recommends \
+#         xvfb \
+#         libjpeg-turbo8-dev \
+#         libgl1-mesa-dev \
+#         libglu1-mesa-dev \
+#         mesa-common-dev \
+#         libfontconfig1-dev \
+#         libicu-dev \
+#         libpng12-dev \
+#         libxslt1-dev \
+#         libxml2-dev \
+#         libhyphen-dev \
+#         libgbm1 \
+#         libxcb-image0 \
+#         libxcb-icccm4 \
+#         libxcb-keysyms1 \
+#         libxcb-render-util0 \
+#         libxi6 \
+#         libxcomposite-dev \
+#         libxrender-dev \
+#         libgstreamer1.0-dev \
+#         libgstreamer-plugins-base1.0-dev \
+#         libgstreamer-plugins-good1.0-dev \
+#         gstreamer1.0-plugins-good \
+#         gstreamer1.0-x \
+#         gstreamer1.0-libav \
+#         webp \
+#         rsync
 }
 
 _ensure_folders () {
