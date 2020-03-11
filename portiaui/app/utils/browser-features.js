fb8bdd20c821f87446b0cfe675195bac0c35e3ba
Phil <phil.behnke@gmail.com>
2020-03-11 00:44:57 -0400

    Update browser-features.js

--- portiaui/app/utils/browser-features.js
+++ portiaui/app/utils/browser-features.js
@@ -9,11 +9,6 @@ export default function hasBrowserFeatures() {
         "csspointerevents", "opacity", "csstransforms", "csstransitions", "cssvhunit",
         "classlist", "placeholder", "localstorage", "svgasimg", "datauri", "atobbtoa"
     ];
-    let feature_promises = features.map((feature) => {
-        return new RSVP.Promise((resolve) => {
-            Modernizr.on(feature, (isFeatureActive) => { resolve(isFeatureActive); });
-        });
-    });
 
-    return RSVP.all(feature_promises);
+    return features;
 }
